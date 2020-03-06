c16fd73bfabfe44cc9c24b2f8d34e04cc3ad1449
laekov <laekov.h@gmail.com>
2020-03-06 17:01:43 +0800

    init body

---
+++ body.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ .$THU_ID = . ]
+then
+	source info.txt
+fi
+
+cat body.json \
+	| sed "s/RB_THU_ID/$RB_THU_ID/g" \
+	| sed "s/RB_DPT_ID/$RB_DPT_ID/g" \
+	| sed "s/RB_DPT_NAME/$RB_DPT_NAME/g" \
+	| sed "s/RB_CHN_NAME/$RB_CHN_NAME/g" \
+	| sed "s/RB_PROVINCE/$RB_PROVINCE/g" \
+	| sed "s/RB_CITY/$RB_CITY/g" \
+	| sed "s/RB_DISTRICT/$RB_DISTRICT/g" \
+	| sed "s/RB_ADDR/$RB_ADDR/g" \
+	>tmp/body.json
