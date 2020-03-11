88631c8e8b19a5947a41d0859ce624e64658b2bf
laekov <laekov.h@gmail.com>
2020-03-11 10:56:41 +0800

    update form of the date

--- body.sh
+++ body.sh
@@ -5,6 +5,8 @@ then
 	source info.txt
 fi
 
+DATE=$(date +%s)000
+
 cat body.json \
 	| sed "s/THU_ID/$THU_ID/g" \
 	| sed "s/RB_DPT_ID/$RB_DPT_ID/g" \
@@ -14,4 +16,5 @@ cat body.json \
 	| sed "s/RB_CITY/$RB_CITY/g" \
 	| sed "s/RB_DISTRICT/$RB_DISTRICT/g" \
 	| sed "s/RB_ADDR/$RB_ADDR/g" \
+	| sed "s/RB_DATE/$DATE/g" \
 	>tmp/body.json
