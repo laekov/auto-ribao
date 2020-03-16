0212cc7e273e9a42c20a9def205ec81244ec56bd
heheda <zhangch99@outlook.com>
2020-03-16 21:34:54 +0800

    write date in file and support retry

--- daemon.sh
+++ daemon.sh
@@ -1,12 +1,25 @@
-#~/bin/bashi
-last=20200315
-while true 
-do
+#!/bin/bash
+mkdir -p tmp
+touch tmp/last
+last=$(cat tmp/last)
+echo "Last submit was at $last"
+while true; do
   cur=`date +%Y%m%d`
   if [ "$last" != "$cur" ]; then
-    date
-    ./autofill.sh
-    last=$cur
+    while true; do
+      ./autofill.sh
+      if [ $? != 0 ]; then
+        echo `date`:" Wait for 1 minute and try"
+        sleep 1m
+      else
+        echo `date`:" Success"
+        echo $cur > tmp/last
+        last=$cur
+        break
+      fi
+    done
+  else
+    echo `date`:" Submitted today"
   fi
   sleep 1h
 done
